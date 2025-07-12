64b31f2888c3a7551edcfbc18347ed48f89be24f
Michael Mwanza <mwanza.n.m@gmail.com>
2025-07-12 23:31:06 +0200

    backend setup

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Routing\UrlGenerator;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(UrlGenerator $url): void
+    {
+        // Force HTTPS in production
+        if (app()->environment('production')) {
+            $url->forceScheme('https');
+        }
+    }
+}
